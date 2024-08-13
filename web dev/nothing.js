6b0dc016861286976d9a38781c16f5eb6df607a1
Herish1605 <herishpatel1112@gmail.com>
2024-08-13 15:55:08 +0530

    practise

--- web dev/nothing.js
+++ web dev/nothing.js
@@ -1,18 +1,22 @@
-let age = prompt("Enter your age");
-age = Number.parseInt(age);  
+
 
 let runAgain = true;
 
 while(runAgain){
+    let age = prompt("Enter your age");
+age = Number.parseInt(age);  
     const canDrive = (age) =>{
         return age>=18 ? true : false
     }
     if(canDrive(age)){
         alert("yes you can drive");
+        
     }
     else{
         alert("you cannot drive"); 
+        
     }
     runAgain= confirm("Do you want to play again?");
 }
+
 
