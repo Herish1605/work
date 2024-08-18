85e1c089ecbaa468bc5ba2c07562602433f488f4
Herish1605 <herishpatel1112@gmail.com>
2024-08-18 10:37:31 +0530

    new topic

--- web dev/nothing.js
+++ web dev/nothing.js
@@ -1,22 +1,40 @@
 
+// program game to ask about licencse 
+// let runAgain = true;
 
-let runAgain = true;
+// while(runAgain){
+//     let age = prompt("Enter your age");
+// age = Number.parseInt(age);  
+//     const canDrive = (age) =>{
+//         return age>=18 ? true : false
+//     }
+//     if(canDrive(age)){
+//         alert("yes you can drive");
+        
+//     }
+//     else{
+//         alert("you cannot drive"); 
+        
+//     }
+//     runAgain= confirm("Do you want to play again?");
+// }
 
-while(runAgain){
-    let age = prompt("Enter your age");
-age = Number.parseInt(age);  
-    const canDrive = (age) =>{
-        return age>=18 ? true : false
-    }
-    if(canDrive(age)){
-        alert("yes you can drive");
-        
-    }
-    else{
-        alert("you cannot drive"); 
-        
-    }
-    runAgain= confirm("Do you want to play again?");
+// const student = {
+//     name : "herish",
+//     id : "23DCE083",
+//     tag: 83,
+//     marks: 100
+// }
+
+// console.log(student.name);
+const marks = {
+    harry: 29,
+    himpadma: 21,
+    meet: 23,
+    hemil: 28
+}
+for(let a in marks){
+    console.log("marks of "+ a + " is "+marks[a]);
 }
 
 
